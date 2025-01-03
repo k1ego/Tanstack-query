1c35de43097fd9fa292fe9b3bea9acb8b9420f1c
k1ego <ladonin05@mail.ru>
2025-01-03 21:09:15 +0300

    Базовый пример запроса

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,3 +1,50 @@
+import { useQuery } from '@tanstack/react-query';
+
+type Todo = {
+	id: string;
+	text: string;
+	done: boolean;
+};
+
+export const getTasks = () => {
+	return new Promise<Todo[]>(res => {
+		setTimeout(() => {
+			res([
+				{
+					id: '1',
+					text: 'todo',
+					done: false,
+				},
+				{
+					id: '2',
+					text: 'todo',
+					done: false,
+				},
+			]);
+		}, 1000);
+	});
+};
+
 export function TodoList() {
-	return <div>Todo List</div>
+	const { data, error, isPending } = useQuery({
+		queryKey: ['tasks', 'list'],
+		queryFn: getTasks,
+	});
+
+	if (isPending) {
+		return <div>Loading</div>;
+	}
+
+	if (error) {
+		return <div>error: {JSON.stringify(error)}</div>;
+	}
+
+	return (
+		<div>
+			Todo List
+			{data.map(todo => (
+				<div key={todo.id}> {todo.text} </div>
+			))}
+		</div>
+	);
 }
