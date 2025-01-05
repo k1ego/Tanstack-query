ac115ef899384224f1957f10500851f18583e59f
k1ego <ladonin05@mail.ru>
2025-01-05 16:43:17 +0300

    Продемонстрирован запрос к серверу для реализации пагинации, с использованием мок json-server

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,10 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { todoListApi } from './api';
 
 export function TodoList() {
-	const { data, error, isPending } = useQuery({
-		queryKey: ['tasks', 'list'],
-		queryFn: todoListApi.getTodoList,
+	const [page, setPage] = useState(1);
+
+	const {
+		data: todoItems,
+		error,
+		isPending,
+	} = useQuery({
+		queryKey: ['tasks', 'list', { page }],
+		queryFn: meta => todoListApi.getTodoList({ page }, meta),
 	});
 
 	if (isPending) {
@@ -19,9 +26,25 @@ export function TodoList() {
 		<div className='p-5 mx-auto max-w-[1200px] mt-10'>
 			<h1 className='text-3xl font-bold underline mb-5'>TodoList</h1>
 			<div className='flex flex-col gap-4'>
-				{data.map(todo => (
-					<div className='border border-slate-300 rounded p-3' key={todo.id}> {todo.text} </div>
+				{todoItems.data.map(todo => (
+					<div className='border border-slate-300 rounded p-3' key={todo.id}>
+						{todo.text}
+					</div>
 				))}
+			</div>
+			<div className='flex gap-2 mt-4'>
+				<button
+					onClick={() => setPage(p => Math.max(p - 1, 1))}
+					className='p-3 rounded border border-teal-500'
+				>
+					prev
+				</button>
+				<button
+					onClick={() => setPage(p => Math.min(p + 1, todoItems.pages))}
+					className='p-3 rounded border border-teal-500'
+				>
+					next
+				</button>
 			</div>
 		</div>
 	);
