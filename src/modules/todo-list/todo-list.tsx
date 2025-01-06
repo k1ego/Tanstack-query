5e1c585d9b56e2bd7a834256759ec0ce813ac8e8
k1ego <ladonin05@mail.ru>
2025-01-06 19:35:12 +0300

    Реализован infinityScroll

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,23 +1,29 @@
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useCallback, useRef, useState } from 'react';
 import { todoListApi } from './api';
 
 export function TodoList() {
-	const [page, setPage] = useState(1);
 	const [enabled, setEnabled] = useState(false);
 
 	const {
 		data: todoItems,
 		error,
-		isPending,
-		isFetching,
 		isLoading,
 		isPlaceholderData,
-	} = useQuery({
-		queryKey: ['tasks', 'list', { page }],
-		queryFn: meta => todoListApi.getTodoList({ page }, meta),
-		placeholderData: keepPreviousData,
-		enabled: enabled
+		fetchNextPage,
+		hasNextPage,
+		isFetchingNextPage,
+	} = useInfiniteQuery({
+		queryKey: ['tasks', 'list'],
+		queryFn: meta => todoListApi.getTodoList({ page: meta.pageParam }, meta),
+		enabled: enabled,
+		initialPageParam: 1,
+		getNextPageParam: result => result.next,
+		select: result => result.pages.flatMap(page => page.data),
+	});
+
+	const cursorRef = useIntersection(() => {
+		fetchNextPage();
 	});
 
 	if (isLoading) {
@@ -37,26 +43,37 @@ export function TodoList() {
 					'flex flex-col gap-4' + (isPlaceholderData ? ' opacity-50' : '')
 				}
 			>
-				{todoItems?.data.map(todo => (
+				{todoItems?.map(todo => (
 					<div className='border border-slate-300 rounded p-3' key={todo.id}>
 						{todo.text}
 					</div>
 				))}
 			</div>
-			<div className='flex gap-2 mt-4'>
-				<button
-					onClick={() => setPage(p => Math.max(p - 1, 1))}
-					className='p-3 rounded border border-teal-500'
-				>
-					prev
-				</button>
-				<button
-					onClick={() => setPage(p => Math.min(p + 1, todoItems?.pages ?? 1))}
-					className='p-3 rounded border border-teal-500'
-				>
-					next
-				</button>
+			<div className='flex gap-2 mt-4' ref={cursorRef}>
+				{!hasNextPage && <div>Нет данных для загрузки</div>}
+				{isFetchingNextPage && <div>...Loading</div>}
 			</div>
 		</div>
 	);
+}
+
+// кастомный хук, который позволяет подписываться на изменение каких либо данных
+export function useIntersection(onIntersect: () => void) {
+	const unsubscribe = useRef(() => {});
+
+	return useCallback((el: HTMLDivElement | null) => {
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(intersection => {
+				if (intersection.isIntersecting) {
+					onIntersect();
+				}
+			});
+		});
+		if (el) {
+			observer.observe(el);
+			unsubscribe.current = () => observer.disconnect();
+		} else {
+			unsubscribe.current();
+		}
+	}, []);
 }
