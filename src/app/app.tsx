6c4fe17a33a251b2845446f1d381004bae9f6f64
k1ego <ladonin05@mail.ru>
2025-01-14 15:30:13 +0300

    Redux Store settings

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,17 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "../shared/api/query-client";
-import { TodoList } from "../modules/todo-list/todo-list";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux';
+import { TodoList } from '../modules/todo-list/todo-list';
+import { queryClient } from '../shared/api/query-client';
+import { store } from '../shared/redux';
 
 export function App() {
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TodoList />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<TodoList />
+			</Provider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
