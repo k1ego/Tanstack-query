b48061c9bde20f5ef1e4888ecae0086c1f5e0514
k1ego <ladonin05@mail.ru>
2025-01-16 17:36:09 +0300

    Реализация offline режима. PWA + Persist storage

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,6 @@
-import { QueryClientProvider } from '@tanstack/react-query';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -7,14 +8,31 @@ import { queryClient } from '../shared/api/query-client';
 import { store } from '../shared/redux';
 import { App } from './app';
 import './index.css';
+import { onlineManager } from '@tanstack/react-query';
+
+// позволяет React Query автоматически реагировать на изменения сетевого статуса (онлайн/оффлайн) и соответствующим образом управлять запросами
+onlineManager.setOnline(navigator.onLine)
+
+const persister = createSyncStoragePersister({
+	storage: window.localStorage,
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
+		<PersistQueryClientProvider
+			client={queryClient}
+			persistOptions={{ persister }}
+			// вызывается когда наступает окончание доставания данных из персистера
+			onSuccess={() => {
+				queryClient.resumePausedMutations().then(() => {
+					queryClient.invalidateQueries();
+				});
+			}}
+		>
 			<Provider store={store}>
 				<App />
 			</Provider>
 			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		</PersistQueryClientProvider>
 	</StrictMode>
 );
